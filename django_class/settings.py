92f0864596e93a2aa4488739a7ea3816f4eded9a
Clovis Caface <clovis.caface@gmail.com>
2023-08-04 12:01:11 -0300

    install apps in settings

--- django_class/settings.py
+++ django_class/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.activities',
+    'apps.class',
+    'apps.core',
+
 ]
 
 MIDDLEWARE = [
